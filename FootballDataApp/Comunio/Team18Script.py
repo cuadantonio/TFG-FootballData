8f33dfc5bdf901f8c6668d747f419a9a312ab33a
Antonio Cuadrado <antoniocuad15@gmail.com>
2022-02-09 11:41:27 +0100

    Comunio scripts
    The rest of the comunio scripts have been made

---
+++ FootballDataApp/Comunio/Team18Script.py
@@ -0,0 +1,25 @@
+import pymongo
+import requests
+from bs4 import BeautifulSoup
+
+team = "Sevilla"
+url = "https://www.comuniazo.com/comunio-apuestas/equipos/sevilla"
+
+client = pymongo.MongoClient("mongodb+srv://root:eODi!SbR5Xqo@cluster0.e9hmo.mongodb.net/footballdata?retryWrites=true&w=majority")
+db = client["footballdata"]
+collection = db["ComunioData"]
+
+page = requests.get(url)
+soup = BeautifulSoup(page.content, "html.parser")
+div = soup.find("div", {"class": "player-list"})
+team_players = div.find_all("li")
+
+for team_player in team_players:
+    playeraux = team_player.find("div", class_="cell")
+    name = playeraux.find_all("strong")[0].text
+    pointsAndPrice = playeraux.find_all("div")[0].text
+    pos = str(pointsAndPrice).find('−')
+    points = str(pointsAndPrice)[0:pos-8]
+    price = str(pointsAndPrice)[pos+2:len(str(pointsAndPrice))]
+    player = {"name": name, "price":price, "points":points, "team": team}
+    collection.insert_one(player)
