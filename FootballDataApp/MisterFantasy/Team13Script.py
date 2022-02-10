b29220953129111e0751c288685ef7406e5533e1
Antonio Cuadrado <antoniocuad15@gmail.com>
2022-02-10 17:55:29 +0100

    MisterFantasy Scripts
    The rest of the misterfantasy scripts are now created

---
+++ FootballDataApp/MisterFantasy/Team13Script.py
@@ -0,0 +1,22 @@
+import pymongo
+import requests
+from bs4 import BeautifulSoup
+
+team = "Mallorca"
+url = "https://mister.mundodeportivo.com/teams/408/mallorca"
+
+client = pymongo.MongoClient("mongodb+srv://root:eODi!SbR5Xqo@cluster0.e9hmo.mongodb.net/footballdata?retryWrites=true&w=majority")
+db = client["footballdata"]
+collection = db["MisterFantasyData"]
+
+page = requests.get(url)
+soup = BeautifulSoup(page.content, "html.parser")
+div = soup.find("div", {"class": "panel-team"})
+team_players = div.find_all("div", {"class": "player-row"})
+
+for team_player in team_players:
+    name = team_player.find("a", {"class": "player"})["data-title"]
+    price = team_player.find("button", {"class": "btn-white"}).text
+    points = team_player.find("div", {"class": "points"}).text
+    player = {"name": name, "price":price, "points":points, "team": team}
+    collection.insert_one(player)
