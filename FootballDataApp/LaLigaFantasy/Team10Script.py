85ca9e53148da15bcba3483d5950ef1c0d591f7e
Antonio Cuadrado <antoniocuad15@gmail.com>
2022-02-11 09:24:56 +0100

    La Liga Fantasy Scripts
    The rest of the la liga fantasy scripts are now created

---
+++ FootballDataApp/LaLigaFantasy/Team10Script.py
@@ -0,0 +1,24 @@
+import pymongo
+import requests
+import json
+
+team = "Getafe"
+url = "https://api.laligafantasymarca.com/api/v3/player/team/9"
+
+client = pymongo.MongoClient("mongodb+srv://root:eODi!SbR5Xqo@cluster0.e9hmo.mongodb.net/footballdata?retryWrites=true&w=majority")
+db = client["footballdata"]
+collection = db["LaLigaFantasyData"]
+
+response = requests.request("GET", url)
+
+data = response.text
+parse_json = json.loads(data)
+json_Response = parse_json['players']
+
+for i in range(len(json_Response)):
+    playeraux = json_Response[i]
+    name = playeraux['nickname']
+    price = playeraux['marketValue']
+    points = playeraux['points']
+    player = {"name": name, "price": price, "points": points, "team": team}
+    collection.insert_one(player)
